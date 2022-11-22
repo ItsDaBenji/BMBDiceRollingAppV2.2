a0bd40ed74ddd22a9a66e1812efd350ed8776a70
itsdabenji <benmblackmon@gmail.con>
2022-11-21 21:13:29 -0600

    Initial Commit

---
+++ BMBDiceRollingAppV2.2/BMBDiceRollingAppV2_2App.swift
@@ -0,0 +1,17 @@
+//
+//  BMBDiceRollingAppV2_2App.swift
+//  BMBDiceRollingAppV2.2
+//
+//  Created by Benjamin Blackmon on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BMBDiceRollingAppV2_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
